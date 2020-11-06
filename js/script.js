9becc80689724ec2369de8e6d5a2288190af234c
chel13 <sanya-sha2016@ukr.net>
2020-11-06 22:43:58 +0200

    06-11-20 2

--- js/script.js
+++ js/script.js
@@ -28,12 +28,32 @@ $(document).ready(function() {
 		$('[data-toggle="tooltip"]').tooltip()
 	} // end if
 
-$(".slider-items-owl").owlCarousel({
-  items:6,
-})
+// $(".slider-items-owl").owlCarousel({
+//   items:6,
+// })
 
 
     
 }); 
 // jquery end
 
+$
+  $('.slider-items-owl').owlCarousel({
+      loop:true, //Зацикливаем слайдер
+      margin:10, //Отступ от картино если выводите больше 1
+      nav:false, //Отключил навигацию
+      autoplay:true, //Автозапуск слайдера
+      smartSpeed:1000, //Время движения слайда
+      autoplayTimeout:2000, //Время смены слайда
+      responsive:{ //Адаптация в зависимости от разрешения экрана
+          0:{
+              items:1
+          },
+          600:{
+              items:4
+          },
+          1000:{
+              items:6
+          }
+      }
+  });
